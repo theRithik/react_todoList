407429ea40674eab3920b21ca57c481113d1c81a
rithik <115656594+theRithik@users.noreply.github.com>
2023-01-23 15:09:35 +0530

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import TodoList from './List';
+
+class TodoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [], text: '' };
+     this.handleChange = this.handleChange.bind(this);
+     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>TODO</h3>
+       
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="new-todo">
+            What needs to be done?
+          </label>
+          <input
+            id="new-todo"
+            onChange={this.handleChange}
+            value={this.state.text}
+          />
+          <button>
+            Add #{this.state.items.length + 1}
+          </button>
+        </form>
+        <TodoList items={this.state.items} />
+      </div>
+    );
+  }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.text.length === 0) {
+      return;
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now()
+    };
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+      text: ''
+    }));
+  }
+}
+
+
+
+export default TodoApp;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TodoApp from './app'
+
+const container= document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<TodoApp/>)
